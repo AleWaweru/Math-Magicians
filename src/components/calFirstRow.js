675a8da8f2540d9c2f185be5f4c001b21c87c6ce
iamalex <ngashalex8@gmail.com>
2023-06-06 15:50:14 +0300

    Add User interface design

---
+++ src/components/calFirstRow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FirthRow = () => (
+  <div className="firth-row">
+    <span className="zero-width">0</span>
+    <span className="dot-width">.</span>
+    <span className="special">=</span>
+  </div>
+);
+export default FirthRow;
