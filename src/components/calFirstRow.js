b44e35fa913650b1fcd52997bc8fdf71026fb9dd
iamalex <ngashalex8@gmail.com>
2023-06-06 16:25:59 +0300

    Change to functional component in Calculator.js

--- src/components/calFirstRow.js
+++ src/components/calFirstRow.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const FirthRow = () => (
-  <div className="firth-row">
-    <span className="zero-width">0</span>
-    <span className="dot-width">.</span>
-    <span className="special">=</span>
-  </div>
-);
+function FirthRow() {
+  return (
+    <div className="firth-row">
+      <span className="zero-width">0</span>
+      <span className="dot-width">.</span>
+      <span className="special">=</span>
+    </div>
+  );
+}
 export default FirthRow;
