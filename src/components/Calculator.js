4c2ba5568e9e64ba7f6575e062ce155584a79d52
iamalex <ngashalex8@gmail.com>
2023-06-07 16:10:58 +0300

    Add Functionality

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,46 +1,9 @@
 import React from 'react';
-import { FaDivide } from 'react-icons/fa';
-
 import './Calculator.css';
-import FirthRow from './calFirstRow';
+import CalcFun from './childCalc';
 
 function Calculator() {
-  return (
-    <div className="calculator-body">
-      <div className="calc-result">
-        <span>0</span>
-      </div>
-      <div className="assign-operators">
-        <div className="first-row">
-          <span>AC</span>
-          <span>+/-</span>
-          <span>%</span>
-          <span className="special" id="division">
-            <FaDivide />
-          </span>
-        </div>
-        <div className="second-row">
-          <span>7</span>
-          <span>8</span>
-          <span>9</span>
-          <span className="special">x</span>
-        </div>
-        <div className="third-row">
-          <span>4</span>
-          <span>5</span>
-          <span>6</span>
-          <span className="special">-</span>
-        </div>
-        <div className="fourth-row">
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span className="special">+</span>
-        </div>
-      </div>
-      <FirthRow />
-    </div>
-  );
+  return <CalcFun />;
 }
 
 export default Calculator;
