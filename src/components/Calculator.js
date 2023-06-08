2eb4699cd2d85c36b225d216bd2b537a8823b6d5
iamalex <ngashalex8@gmail.com>
2023-06-08 12:53:31 +0300

    Add display Quote

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Calculator.css';
 import CalcFun from './childCalc';
+import Quotes from './quote';
 
 function Calculator() {
-  return <CalcFun />;
+  return (
+    <>
+      <CalcFun />
+      <Quotes />
+    </>
+  );
 }
 
 export default Calculator;
